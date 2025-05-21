5b88e4debd21248e682d04e830ed5e8c6a8dc7b1
Theja <sugamanch@gmail.com>
2025-05-21 14:48:44 +0530

    Tested cookies

--- classroom/server.js
+++ classroom/server.js
@@ -2,7 +2,33 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js")
 const posts = require("./routes/post.js");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser("secretcode"));
 
+
+app.get("/getsignedcookie",(req,res)=>{
+    res.cookie("madi_in","India",{signed: true});
+    res.send("Signed cookie send ");
+});
+
+app.get("/verify",(req,res)=>{
+    console.log(req.signedCookies);
+    res.send("Verified");
+})
+app.get("/greet", (req,res)=>{
+    let {name = "anonymous"} = req.cookies;
+    res.send(`Hii ${name}`);
+})
+
+app.get("/getcookies",(req,res)=>{
+    res.cookie("greet","namaste");
+    res.cookie("madeIn","india");
+  res.send("Send you some cookies");
+});
+app.get("/",(req,res)=>{
+    console.dir(req.cookies);
+    res.send("I am root");
+})
 app.use("/users",users);
 app.use("/posts",posts);
 

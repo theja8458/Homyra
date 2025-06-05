83384c40c903ac541aa76b00ea463a45cf431b37
Theja <sugamanch@gmail.com>
2025-06-05 13:33:09 +0530

    Added Map popup

--- public/JavaScript/map.js
+++ public/JavaScript/map.js
@@ -1,11 +1,18 @@
-	mapboxgl.accessToken = mapToken;
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 9 // starting zoom
-    });
+mapboxgl.accessToken = mapToken;
 
-     console.log(coordinates);
-     const marker1 = new mapboxgl.Marker({color: "red"})
-        .setLngLat(coordinates) //Listing.geometry.coordinates
-        .addTo(map);
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: "mapbox://styles/mapbox/streets-v12",
+  center: listing.geometry.coordinates,
+  zoom: 9
+});
+
+console.log(listing.geometry.coordinates);
+
+const marker1 = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 })
+      .setHTML(`<h4>${listing.location}</h4><p>Exact Location will be  Provided after booking!</p>`)
+  )
+  .addTo(map);

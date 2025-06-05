f85fd4d0edda8ea97a53fa9050340ff7168e315b
Theja <sugamanch@gmail.com>
2025-06-05 13:09:21 +0530

    Used geocoding concept

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,8 @@
 const Listing = require("../models/listing");
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
 
 module.exports.index = async (req,res,next)=>{
 const allListings =  await Listing.find({});
@@ -22,6 +26,12 @@ module.exports.showListing = async (req, res,next) => {
 };
 
 module.exports.createListing = async (req,res,next)=>{
+ let response = await  geocodingClient.forwardGeocode({
+  query: req.body.listing.location,
+  limit: 1
+})
+  .send();
+
   let url = req.file.path;
   let filename = req.file.filename;
   // console.log(url, ".." , filename);
@@ -32,9 +42,11 @@ module.exports.createListing = async (req,res,next)=>{
             url: url, 
             filename: filename, 
         },
-        owner : req.user._id
+        owner : req.user._id,
+        geometry :response.body.features[0].geometry,
     });
-    await newListing.save();
+   let savedListing =  await newListing.save();
+   console.log(savedListing);
     req.flash("success","New Listing Created!");
   res.redirect("/listings");
 };
--- public/JavaScript/map.js
+++ public/JavaScript/map.js
@@ -1,6 +1,11 @@
 	mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        center: [77.2090, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
+
+     console.log(coordinates);
+     const marker1 = new mapboxgl.Marker({color: "red"})
+        .setLngLat(coordinates) //Listing.geometry.coordinates
+        .addTo(map);
